9c1df04e7d86191761da3c5120f0d59812cec437
sebanime02 <sebastianbell2@hotmail.com>
2014-10-16 23:49:59 -0500

    v0.1

---
+++ gen/sebanime02/unicauca/rgb/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package sebanime02.unicauca.rgb;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/sebanime02/unicauca/rgb/MainActivity.java
@@ -0,0 +1,31 @@
+package sebanime02.unicauca.rgb;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+        
+    }
+
+  @Override
+protected void onRestart() {
+	// TODO Auto-generated method stub
+	super.onRestart();
+}
+ 
+  @Override
+protected void onDestroy() {
+	// TODO Auto-generated method stub
+	super.onDestroy();
+}
+    
+}
